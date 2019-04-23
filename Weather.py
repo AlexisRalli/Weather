479405f64eea5967ef306558cdc4f676e7aac8e0
AlexisRalli <rallilex@hotmail.co.uk>
2019-04-23 17:01:42 +0100

    manual city input

--- Weather.py
+++ Weather.py
@@ -3,21 +3,30 @@ import requests
 
 
 key = '623bafaf516827fba2b5a2e1c329b274'
-city = 'Cleveland'
+Web_Url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID='
+#city = 'Cleveland'
 
-url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID='
-url = url + key + '&q=' + city
+Keep_Going=True
+while Keep_Going:
+    try:
+        city=input('Please enter a city: ')   
+        url = Web_Url + key + '&q=' + city   
+        json_data = requests.get(url).json()
+        json_data = json_data['list'][0] #need zero index, as comes as dictionary in list
+        weather = json_data['weather'][0]
+        Keep_Going=False
+    except:
+        print('NOT a real city')
+        continue
 
-json_data = requests.get(url).json()
-with open('data.json', 'w') as outfile:
-    json.dump(json_data, outfile)
-    
+
+#with open('data.json', 'w') as outfile:  #print json data
+#    json.dump(json_data, outfile)
+#
 #print(json_data)
 #print(json_data.keys())
 #print(json_data['list'][0])
 
-json_data = json_data['list'][0] #need zero index, as comes as dictionary in list
-weather = json_data['weather'][0]
 
 #Main = json_data['weather'][0]['main']
 Description = json_data['weather'][0]['description']
@@ -27,8 +36,8 @@ print('-->', Description, '<--') #Not printing Main
 try:
     Rain = json_data['rain']
     if len(Rain) > 0:
-        for key, value in Rain:
-            print('It will rain:', key, value, 'mm')
+        for time in Rain.keys():
+            print('It will rain:', Rain[time], 'mm, for:', time)
     elif len(Rain) == 0:
         print('No rain today!!!')
 except:
